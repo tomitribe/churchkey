2e79bb0961a7f5908cd5bc152aec08a22295398e
dblevins <david.blevins@gmail.com>
2018-09-30 00:50:35 -0700

    Clean BEGIN PUBLIC KEY support for RSA

--- src/main/java/org/tomitribe/churchkey/PemDecoder.java
+++ src/main/java/org/tomitribe/churchkey/PemDecoder.java
@@ -21,27 +21,17 @@ import org.tomitribe.churchkey.pem.BeginRsaPrivateKey;
 import org.tomitribe.churchkey.pem.BeginRsaPublicKey;
 import org.tomitribe.util.Base64;
 
-import java.io.IOException;
-import java.math.BigInteger;
-import java.security.KeyFactory;
-import java.security.NoSuchAlgorithmException;
-import java.security.PublicKey;
-import java.security.interfaces.DSAPublicKey;
-import java.security.interfaces.ECPublicKey;
-import java.security.interfaces.RSAPublicKey;
-import java.security.spec.InvalidKeySpecException;
-import java.security.spec.RSAPublicKeySpec;
-import java.security.spec.X509EncodedKeySpec;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Function;
 
 public class PemDecoder implements Decoder {
 
-    private final Map<String, Function<byte[],Key>> converters = new HashMap<>();
+    private final Map<String, Function<byte[], Key>> converters = new HashMap<>();
+
     {
         converters.put("PRIVATE KEY", this::unimplemented);
-        converters.put("PUBLIC KEY", this::publicKey);
+        converters.put("PUBLIC KEY", BeginPublicKey::decode);
         converters.put("RSA PRIVATE KEY", BeginRsaPrivateKey::decode);
         converters.put("RSA PUBLIC KEY", BeginRsaPublicKey::decode);
         converters.put("DSA PRIVATE KEY", this::unimplemented);
@@ -75,53 +65,6 @@ public class PemDecoder implements Decoder {
         return converter.apply(bytes);
     }
 
-
-    private Key publicKey(final byte[] key) {
-
-        try {
-            final KeyFactory result;
-            try {
-                result = KeyFactory.getInstance("RSA");
-            } catch (NoSuchAlgorithmException e) {
-                throw new IllegalStateException(e);
-            }
-            final KeyFactory factory = result;
-            final RSAPublicKey publicKey = (RSAPublicKey) factory.generatePublic(new X509EncodedKeySpec(key));
-            return new Key(publicKey, Key.Type.PUBLIC, Key.Algorithm.RSA, Key.Format.PEM);
-        } catch (InvalidKeySpecException e) {
-            // continue trying other algorithms
-        }
-
-        try {
-            final KeyFactory result;
-            try {
-                result = KeyFactory.getInstance("DSA");
-            } catch (NoSuchAlgorithmException e) {
-                throw new IllegalStateException(e);
-            }
-            final KeyFactory factory = result;
-            final DSAPublicKey publicKey = (DSAPublicKey) factory.generatePublic(new X509EncodedKeySpec(key));
-            return new Key(publicKey, Key.Type.PUBLIC, Key.Algorithm.DSA, Key.Format.PEM);
-        } catch (InvalidKeySpecException e) {
-            // continue trying other algorithms
-        }
-
-        try {
-            final KeyFactory result;
-            try {
-                result = KeyFactory.getInstance("EC");
-            } catch (NoSuchAlgorithmException e) {
-                throw new IllegalStateException(e);
-            }
-            final KeyFactory factory = result;
-            final ECPublicKey publicKey = (ECPublicKey) factory.generatePublic(new X509EncodedKeySpec(key));
-            return new Key(publicKey, Key.Type.PUBLIC, Key.Algorithm.EC, Key.Format.PEM);
-        } catch (InvalidKeySpecException e) {
-            // continue trying other algorithms
-        }
-
-        throw new UnsupportedOperationException("Unsupported algorithm or invalid x509 key spec");
-    }
 
     private Key unimplemented(final byte[] key) {
         throw new UnsupportedOperationException();
--- src/main/java/org/tomitribe/churchkey/pem/BeginPublicKey.java
+++ src/main/java/org/tomitribe/churchkey/pem/BeginPublicKey.java
@@ -18,10 +18,48 @@ package org.tomitribe.churchkey.pem;
 
 import org.tomitribe.churchkey.Key;
 
+import java.security.KeyFactory;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.DSAPublicKey;
+import java.security.interfaces.ECPublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.X509EncodedKeySpec;
+
 public class BeginPublicKey {
 
     public static Key decode(final byte[] bytes) {
-        return null;
+        try {
+            final KeyFactory factory = KeyFactory.getInstance("RSA");
+            final RSAPublicKey publicKey = (RSAPublicKey) factory.generatePublic(new X509EncodedKeySpec(bytes));
+            return new Key(publicKey, Key.Type.PUBLIC, Key.Algorithm.RSA, Key.Format.PEM);
+        } catch (InvalidKeySpecException e) {
+            // continue trying other algorithms
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+
+        try {
+            final KeyFactory factory = KeyFactory.getInstance("DSA");
+            final DSAPublicKey publicKey = (DSAPublicKey) factory.generatePublic(new X509EncodedKeySpec(bytes));
+            return new Key(publicKey, Key.Type.PUBLIC, Key.Algorithm.DSA, Key.Format.PEM);
+        } catch (InvalidKeySpecException e) {
+            // continue trying other algorithms
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+
+        try {
+            final KeyFactory factory = KeyFactory.getInstance("EC");
+            final ECPublicKey publicKey = (ECPublicKey) factory.generatePublic(new X509EncodedKeySpec(bytes));
+            return new Key(publicKey, Key.Type.PUBLIC, Key.Algorithm.EC, Key.Format.PEM);
+        } catch (InvalidKeySpecException e) {
+            // continue trying other algorithms
+        } catch (NoSuchAlgorithmException e) {
+            throw new IllegalStateException(e);
+        }
+
+        throw new UnsupportedOperationException("Unsupported algorithm or invalid x509 key spec");
     }
 
     public static byte[] encode(final Key key) {
